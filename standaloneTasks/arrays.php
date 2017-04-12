dbe619f5efa011835ce160ced4d5c6449184f580
Aleksandrs <sanjkagm@inbox.lv>
2017-04-12 22:22:51 +0300

    task done

---
+++ standaloneTasks/arrays.php
@@ -0,0 +1,10 @@
+<?php
+    $arr = range(0,100);
+    $output = array_rand($arr,3);
+    echo 'Here are random numbers:';
+    print_r($output);
+    $result = 0;
+    foreach($output as $sum){
+        $result += $sum;
+    }
+    echo "\n".'sum is :'.$result;
