7a9ee5d23b31a5f6b68cca3c21ca448ff70ecb83
Aleksandrs <sanjkagm@inbox.lv>
2017-04-19 00:03:56 +0300

    Minor changes

--- standaloneTasks/input_concat.php
+++ standaloneTasks/input_concat.php
@@ -7,6 +7,7 @@
     $line1 = readline('Enter string 1: ');
     $line2 = readline('Enter string 2: ');
     
+    echo "\nThe type and sum of two entered values is: ";
     var_dump( $result);
-    echo "\n".'result powered by 2: '.($result**2)."\n";
-    echo $line1.$line2;
+    echo 'Result powered by 2: '.($result**2)."\n";
+    echo 'New concatenated string is: '.$line1.$line2;
